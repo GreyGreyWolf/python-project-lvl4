59ae9dfa0dd269eefdfa7419965a65af4bfa8366
GreyGreyWolf <wolfdieattheend@yandex.ru>
2020-09-24 14:46:52 +0300

    Add dotenv

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -10,22 +10,26 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
+import os
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...).
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DOTENV_FILE = os.path.join(BASE_DIR, ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3p+hvprjyn^yozaq-qdd7&3!6y$)%@j5x!i@+g6q4#bw37&ovb'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv('DEBUG', False))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -129,3 +133,4 @@ ROLLBAR = {
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
