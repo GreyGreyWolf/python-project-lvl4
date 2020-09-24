ac718354c97ea925d4c3643a694bb1fe96164779
GreyGreyWolf <wolfdieattheend@yandex.ru>
2020-09-24 19:23:06 +0300

    Update

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -131,3 +131,6 @@ ROLLBAR = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
