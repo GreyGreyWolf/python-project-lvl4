70f5c6dc3c6490dd7013c560bab917d3d0e2d5e9
GreyGreyWolf <wolfdieattheend@yandex.ru>
2020-10-05 17:43:43 +0300

    v1

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'task_manager',
     'bootstrap4',
+    'users',
+    'tasks',
 ]
 
 MIDDLEWARE = [
@@ -132,5 +134,9 @@ ROLLBAR = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media/status_image')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url, include
 from django.urls import path
 from task_manager import views
 
+
 urlpatterns = [
     path('', views.index),
-    path('admin/', admin.site.urls),
+    url(r'admin/', admin.site.urls),
+    url(r'^', include('users.urls')),  
 ]
