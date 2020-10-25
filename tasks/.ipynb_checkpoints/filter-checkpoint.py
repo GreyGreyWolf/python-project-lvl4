030d9228002ce0008a0f803875a8062045684682
GreyGreyWolf <57860055+GreyGreyWolf@users.noreply.github.com>
2020-10-25 17:26:04 +0300

    Delete filter-checkpoint.py

--- tasks/.ipynb_checkpoints/filter-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import django_filters
-from tasks.models import Task
-
-
-class TaskFilter(django_filters.FilterSet):
-    class Meta:
-        model = Task
-        fields = ['assigned_to', 'tags', 'status']
