278424744c789158daa0e25bf210c214751e082a
GreyGreyWolf <woldieattheend@uandex.ru>
2020-10-27 22:56:36 +0300

    Fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -85,8 +85,10 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 # Database
 DATABASES = {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 # Use the DATABASE_URL environment variable
