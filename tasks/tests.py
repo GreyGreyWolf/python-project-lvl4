79cbbb1b2b161f754c654fa3b3fa6e075d28d250
GreyGreyWolf <woldieattheend@uandex.ru>
2020-10-27 21:37:32 +0300

    Some fix

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,35 +1,34 @@
-from django.contrib.auth.models import User
-from django.test import Client, RequestFactory, TestCase
+from django.template.response import TemplateResponse
+from django.urls import reverse_lazy
+from django.test import Client, TestCase
+from auth.models import SiteUser
+from faker import Faker
+from faker.generator import Generator
 
-from tasks import views
-from tasks.models import Tag, Task, Taskstatus
 
-
-class TaskTest(TestCase):
+class SuccessRegistrationTest(TestCase):
 
     def setUp(self):
-        self.factory = RequestFactory()
-        self.user = User.objects.create_user(
-            username='testuser',
-            password='1q2w3e4r5T',
-        )
-        self.client = Client()
+        self.client: Client = Client()
+        self.faker: Generator = Faker()
 
-    def createTask(self, name="test_name"):
-        test_status = Taskstatus.objects.create(name='test')
-        test_tag = Tag.objects.create(name='Test')
-        test_task = Task.objects.create(
-            name=name,
-            description='Description for test',
-            assigned_to=self.user,
-            creator=self.user,
-            status=test_status,
+    def test(self):
+        email: str = self.faker.email()
+        fake_password: str = self.faker.password(length=10)
+        user_name: str = self.faker.user_name()
+        response: TemplateResponse = self.client.post(
+            reverse_lazy('auth:registration'),
+            data={
+                'email': email,
+                'username': user_name,
+                'password1': fake_password,
+                'password2': fake_password,
+            },
         )
-        test_task.tags.set([test_tag])
-        return test_task
+        self.assertRedirects(response, reverse_lazy('login'))
+        self.assertTrue(
+            SiteUser.objects.filter(email=email, username=user_name),
+        )
 
-    def test_task_create(self):
-        test_task = self.createTask()
-        self.assertTrue(isinstance(test_task, Task))
-        self.assertEqual(test_task.__str__(), test_task.name)
-        self.assertEqual(Task.objects.count(), 1)
+
+
