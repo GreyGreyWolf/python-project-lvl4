5d4e11324c60b6019c83d37f6d6fedf0d255cf32
GreyGreyWolf <woldieattheend@uandex.ru>
2020-10-27 22:41:31 +0300

    Search error

--- tasks/tests.py
+++ tasks/tests.py
@@ -4,18 +4,18 @@ from faker.generator import Generator
 from tasks.models import Tag, Taskstatus
 
 
-class TagModelTest(TestCase):
+class TaskstatusModelTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        Tag.object.create(name='Test')
+        Taskstatus.object.create(name='Teststatus')
 
     def test_tag_name_label(self):
-        tag = Tag.objects.get(id=1)
-        field_label = tag._meta.get_field('name').verbose_name
+        taskstatus = Taskstatus.objects.get(id=1)
+        field_label = taskstatus._meta.get_field('name').verbose_name
         self.assertEquals(field_label, 'name')
 
     def test_tag_name_max_length(self):
-        tag = Tag.objects.get(id=1)
-        max_length = tag._meta.get_field('name').max_length
-        self.assertEquals(max_length, 20)
+        taskstatus = Taskstatus.objects.get(id=1)
+        max_length = taskstatus._meta.get_field('name').max_length
+        self.assertEquals(max_length, 100)
