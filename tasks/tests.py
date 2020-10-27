ee690c1d9e784e45ff2789b0ae2546f550037223
GreyGreyWolf <woldieattheend@uandex.ru>
2020-10-27 20:51:46 +0300

    Fix

--- tasks/tests.py
+++ tasks/tests.py
@@ -8,38 +8,28 @@ from tasks.models import Tag, Task, Taskstatus
 class TaskTest(TestCase):
 
     def setUp(self):
-        """Initial"""
         self.factory = RequestFactory()
         self.user = User.objects.create_user(
-            username='user',
+            username='testuser',
             password='1q2w3e4r5T',
         )
         self.client = Client()
 
-    def createTask(self):
-        """Create task"""
-        status = Taskstatus.objects.create(name='Status for test')
-        tag = Tag.objects.create(name='Tag for test')
-        task = Task.objects.create(
-            name='Name for test',
+    def createTask(self, name="test_name"):
+        test_status = Taskstatus.objects.create(name='test')
+        test_tag = Tag.objects.create(name='Test')
+        test_task = Task.objects.create(
+            name=name,
             description='Description for test',
             assigned_to=self.user,
             creator=self.user,
-            status=status,
+            status=test_status,
         )
-        task.tags.add(tag)
-        return task
+        test_task.tags.set([test_tag])
+        return test_task
 
     def test_task_create(self):
-        """Test task creation."""
-        task = self.createTask()
-        self.assertTrue(isinstance(task, Task))
-        self.assertEqual(task.__str__(), task.name)
+        test_task = self.createTask()
+        self.assertTrue(isinstance(test_task, Task))
+        self.assertEqual(test_task.__str__(), test_task.name)
         self.assertEqual(Task.objects.count(), 1)
-
-    def test_tasks_view(self):
-        """Test tasks view"""
-        request = self.factory.get('/')
-        request.user = self.user
-        response = views.TasksViews.as_view()(request)
-        self.assertEqual(response.status_code, 200)
