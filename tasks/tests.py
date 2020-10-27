3bd103015b671dc01ea6ff2ba97108e9858b1301
GreyGreyWolf <woldieattheend@uandex.ru>
2020-10-27 22:29:49 +0300

    New test

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,23 +1,21 @@
-from django.template.response import TemplateResponse
-from django.urls import reverse_lazy
-from django.test import Client, TestCase
+from django.test import TestCase
 from faker import Faker
 from faker.generator import Generator
-from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
+from tasks.models import Tag, Taskstatus
 
 
-class SigninTest(TestCase):
+class TagModelTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        User.objects.create_user(
-            username='test', password='12test12', email='test@example.com')
+        Tag.object.create(name='Test')
 
-    def test_wrong_username(self):
-        user = authenticate(username='wrong', password='12test12')
-        self.assertFalse(user is not None and user.is_authenticated)
+    def test_tag_name_label(self):
+        tag = Tag.objects.get(id=1)
+        field_label = tag._meta.get_field('name').verbose_name
+        self.assertEquals(field_label, 'name')
 
-    def test_wrong_password(self):
-        user = authenticate(username='test', password='wrong')
-        self.assertFalse(user is not None and user.is_authenticated)
+    def test_tag_name_max_length(self):
+        tag = Tag.objects.get(id=1)
+        max_length = tag._meta.get_field('name').max_length
+        self.assertEquals(max_length, 20)
