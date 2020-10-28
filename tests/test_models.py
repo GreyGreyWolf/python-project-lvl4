d8064687a2b9819638d0a296d6780c7b14c54346
GreyGreyWolf <woldieattheend@uandex.ru>
2020-10-28 19:05:22 +0300

    add test tasks

--- tests/test_models.py
+++ tests/test_models.py
@@ -49,38 +49,44 @@ class TagModelTest(TestCase):
         self.assertEquals(expected_object_name, str(tag))
 
 
-class TasksModelTest(TestCase):
+class TaskModelTest(TestCase):
 
-    @classmethod
-    def setUpTestData(cls):
-        tags = ''
-        Taskstatus.objects.create(name='Teststatus')
-        User.objects.create(username='Teststatus')
-        Tag.objects.create(name='test')
-        name_tag = Tag.objects.get(id=1)
-        Task.objects.create(name='Testtask',
-                            description='test',
-                            status=Taskstatus.objects.get(id=1),
-                            creator=User.objects.get(id=1),
-                            assigned_to=User.objects.get(id=1),
-                            tags.set(Tag.objects.name))
+    def setUp(self):
+        self.user1 = User.objects.create_user(
+            username='test1', password='1q2w3e4r', email='test@example.com')
+        self.user2 = User.objects.create_user(
+            username='test2', password='1q2w3e4r', email='test@example.com')
+        self.user1.save()
+        self.user2.save()
+        self.status = Taskstatus.objects.create(name='test')
+        self.status.save()
+        self.task = Task(name='TestTask',
+                         description='description',
+                         status=self.status,
+                         creator=self.user1,
+                         assigned_to=self.user2)
+        self.task.save()
 
-    def test_task_name_label(self):
-        task = Task.objects.get(id=1)
-        field_label = task._meta.get_field('name').verbose_name
-        self.assertEquals(field_label, 'name')
+    def tearDown(self):
+        self.user1.delete()
+        self.user2.delete()
 
-    def test_taskstatus_name_max_length(self):
-        task = Task.objects.get(id=1)
-        max_length = task._meta.get_field('name').max_length
-        self.assertEquals(max_length, 100)
+    def test_name_max_length(self):
+        max_length = self.task._meta.get_field('name').max_length
+        self.assertEquals(max_length, 50)
 
-    def test_object_name(self):
-        task = Task.objects.get(id=1)
-        expected_object_name = '%s, %s, %s, %s, %s, %s' % (task.name,
-                                                           task.description,
-                                                           Taskstatus,
-                                                           task.creator,
-                                                           task.task.assigned_to,
-                                                           task.tags)
-        self.assertEquals(expected_object_name, str(task))
+    def test_task_name(self):
+        self.assertEquals(self.task.name, str(self.task))
+
+    def test_read_task(self):
+        self.assertEqual(self.task.status, self.status)
+        self.assertEqual(self.task.creator, self.user1)
+        self.assertEqual(self.task.assigned_to, self.user2)
+        self.assertEqual(self.task.description, 'description')
+
+    def test_update_task_(self):
+        self.task.description = 'new description'
+        self.task.assigned_to = self.user1
+        self.task.save()
+        self.assertEqual(self.task.description, 'new description')
+        self.assertEqual(self.task.assigned_to, self.user1)
